e4cd7caa29710066e4bab8919b25a2628555e285
Matthew Lee <matthewlee0725@gmail.com>
2023-07-31 15:49:10 +0800

    chore: looks like we don't need arq for now

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from arq import create_pool
-from arq.connections import RedisSettings
-from arq.typing import WorkerSettingsBase
 from quart import Quart, json
 from quart_cors import cors
 from werkzeug.exceptions import HTTPException
@@ -10,13 +7,6 @@ from logger import logger
 
 app = Quart(__name__)
 app = cors(app, allow_origin='*')
-
-
-# https://pgjones.gitlab.io/quart/how_to_guides/startup_shutdown.html
-@app.before_serving
-async def before_serving():
-    logger.info(f'create arq in app before serving')
-    app.arq = await create_pool(RedisSettings())
 
 
 # https://flask.palletsprojects.com/en/2.2.x/errorhandling/#generic-exception-handler
@@ -35,20 +25,3 @@ def handle_exception(e: HTTPException):
     response.content_type = APPLICATION_JSON
     logger.error(f'errorhandler, data={response.data}')
     return response
-
-
-# ctx is arq first param, keep it.
-async def do_on_arq_worker_startup(ctx: dict):
-    logger.info(f'arq worker startup')
-
-
-# ctx is arq first param, keep it.
-async def do_on_arq_worker_shutdown(ctx: dict):
-    logger.info(f'arq worker shutdown')
-
-
-# https://arq-docs.helpmanual.io/#simple-usage
-class WorkerSettings(WorkerSettingsBase):
-    functions = []  # TODO (Matthew Lee) add some tasks...
-    on_startup = do_on_arq_worker_startup
-    on_shutdown = do_on_arq_worker_shutdown
