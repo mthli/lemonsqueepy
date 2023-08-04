3ddde6f3318f9a25ab16469214bed390672c93fe
Matthew Lee <matthewlee0725@gmail.com>
2023-08-04 11:45:40 +0800

    Revert "feat: define lemonsqueezy_api_key"
    This reverts commit 47a9f6e75eb69e14a173b9ab133d43acb0248068.

--- rds.py
+++ rds.py
@@ -18,10 +18,6 @@ GOOGLE_OAUTH_CLIENT_IDS = 'google_oauth_client_ids'  # set.
 # for example "0123456789abcdef" (don't use it, just a example, haha).
 LEMONSQUEEZY_SIGNING_SECRET = 'lemonsqueezy_signing_secret'  # string.
 
-# For fetching some data from lemonsqueezy.
-# https://docs.lemonsqueezy.com/api#authentication
-LEMONSQUEEZY_API_KEY = 'lemonsqueezy_api_key'  # string.
-
 # Default host and port.
 rds = redis.from_url('redis://localhost:6379')
 
